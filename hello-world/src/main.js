f4066426386b1a07998215e49821bfd53ee44140
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-26 12:06:48 -0500

    Add and position the camera

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -16,3 +16,15 @@ console.log(scene)
 scene.add(cubeMesh)
 
 console.log(scene)
+
+// Initialize the camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    30
+)
+
+// Position the camera
+camera.position.z = 5
+scene.add(camera)
