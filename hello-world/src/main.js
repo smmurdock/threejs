5d4316a32818b2e2bb9b109e3a90e9d06120bc88
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-26 11:55:37 -0500

    Initialize project

---
+++ hello-world/src/main.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1,)
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" })
+
+const cubeMesh = new THREE.Mesh(
+    cubeGeometry,
+    cubeMaterial
+)
+
+console.log(cubeMesh)
+console.log(scene)
